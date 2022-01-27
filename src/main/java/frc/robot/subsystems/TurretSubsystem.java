8a9651d2b8c2ad7b1b971e7ffd61e910a4b77705
Shashhank12 <59421940+Shashhank12@users.noreply.github.com>
2022-01-27 14:17:10 -0800

    Finished(Needs tessting)

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -7,16 +7,15 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController.AccelStrategy;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class TurretSubsystem extends SubsystemBase {
   /** Creates a new TurretSubsystem. */
-  public TurretSubsystem() 
-  {
-    private static final int TurretPort = 0;
 
-  CANSparkMax turretCanSparkMax = new CANSparkMax(TurretPort, MotorType.kBrushless);
+  CANSparkMax turretCanSparkMax = new CANSparkMax(Constants.TurretMotorPort, MotorType.kBrushless);
 
   public static final int TurretPIDSlot = 0;
   public static final int LeftSoftLimit = 0;
@@ -40,11 +39,12 @@ public class TurretSubsystem extends SubsystemBase {
 
   public static final double SmartMotionMaxAcceleration = 50;
   public static final double SmartMotionMaxVelocity = 100;
+
   //4096 represents 1 revolution
   public static final int TurretPosition = 4096 * 1;
 
   /** Creates a new Turret. */
-  public Turret() 
+  public TurretSubsystem() 
   {
     turretCanSparkMax.getPIDController().setFF(TurretFF);
     turretCanSparkMax.getPIDController().setP(TurretP);
@@ -52,37 +52,59 @@ public class TurretSubsystem extends SubsystemBase {
     turretCanSparkMax.getPIDController().setD(TurretD);
 
     turretCanSparkMax.getPIDController().setOutputRange(TurretOutputMin, TurretOutputMax);
-    turretCanSparkMax.getPIDController().setSmartMotionAllowedClosedLoopError(ClosedLoopError, TurretPIDSlot);
-    
-    //turretCanSparkMax.getEncoder().setPosition(TurretPosition);
 
+    turretCanSparkMax.getPIDController().setSmartMotionAllowedClosedLoopError(ClosedLoopError, TurretPIDSlot);
+    turretCanSparkMax.getPIDController().setSmartMotionAccelStrategy(AccelStrategy.kSCurve, TurretPIDSlot);
     turretCanSparkMax.getPIDController().setSmartMotionMaxAccel(SmartMotionMaxAcceleration, TurretPIDSlot);
     turretCanSparkMax.getPIDController().setSmartMotionMaxVelocity(SmartMotionMaxVelocity, TurretPIDSlot);
 
     turretCanSparkMax.getPIDController().setReference(0, ControlType.kSmartMotion);
-    
-    
   }
+
   public void turretCanTurn(double position)
+  {
+    if((LeftSoftLimit == turretCanSparkMax.getEncoder().getPosition()) && (RightSoftLimit == turretCanSparkMax.getEncoder().getPosition()))
     {
-      if((LeftSoftLimit == turretCanSparkMax.getEncoder().getPosition()) && (RightSoftLimit == turretCanSparkMax.getEncoder().getPosition()))
-      {
-        turretCanSparkMax.getPIDController().setReference(0, ControlType.kDutyCycle);
-      }
-      else
-      {
-        turretCanSparkMax.getPIDController().setReference(position, ControlType.kPosition);
-      }
+      turretCanSparkMax.getPIDController().setReference(0, ControlType.kDutyCycle, TurretPIDSlot);
     }
+    else
+    {
+      turretCanSparkMax.getPIDController().setReference(position, ControlType.kPosition, TurretPIDSlot);
+    }
+  }
+
   public void turretTurnLeft()
   {
     if(LeftSoftLimit != turretCanSparkMax.getEncoder().getPosition())
     {
-      turretCanSparkMax.getPIDController().setReference(0.1, ControlType.kDutyCycle);
+      turretCanSparkMax.getPIDController().setReference(-0.1, ControlType.kDutyCycle, TurretPIDSlot);
+    }
+    else
+    {
+      turretCanSparkMax.getPIDController().setReference(0, ControlType.kDutyCycle, TurretPIDSlot);
     }
   }
+
+  public void turretTurnRight()
+  {
+    if(RightSoftLimit != turretCanSparkMax.getEncoder().getPosition())
+    {
+      turretCanSparkMax.getPIDController().setReference(0.1, ControlType.kDutyCycle, TurretPIDSlot);
+    }
+    else
+    {
+      turretCanSparkMax.getPIDController().setReference(0, ControlType.kDutyCycle, TurretPIDSlot);
+    }
+  }
+
+  public void stopTurret()
+  {
+    turretCanSparkMax.getPIDController().setReference(0, ControlType.kDutyCycle);
+  }
+
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void periodic() 
+  {
+    System.out.println(turretCanSparkMax.getEncoder().getPosition());
   }
 }
