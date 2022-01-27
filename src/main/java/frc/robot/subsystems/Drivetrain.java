85af77ed7df80cfd51ec8d15e30bc064fb2e56ab
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2022-01-27 13:45:06 -0800

    PID - needs a bit more testing

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -16,7 +16,40 @@ public class Drivetrain extends SubsystemBase
   CANSparkMax bottomleftMotor = new CANSparkMax(Constants.robotPortBottomLeft, MotorType.kBrushless);
   CANSparkMax topRightMotor = new CANSparkMax(Constants.robotPortTopRight, MotorType.kBrushless);
   CANSparkMax bottomRightMotor = new CANSparkMax(Constants.robotPortBottomRight, MotorType.kBrushless);
-  public Drivetrain() {}
+  // change PID values later, can't test rn
+  private final double CanSparkMotorP = 0.0000005;
+  private final double CanSparkMotorI= 0.00000000001;
+  private final double CanSparkMotorD= 0.0000003;
+  public Drivetrain() {
+
+  topleftMotor.getPIDController().setP(CanSparkMotorP);
+  topleftMotor.getPIDController().setI(CanSparkMotorI);
+  topleftMotor.getPIDController().setD(CanSparkMotorD);
+  topleftMotor.getPIDController().setOutputRange(-0.5, 0.5);
+  topleftMotor.getEncoder().setPosition(0);
+  topleftMotor.getEncoder().setPositionConversionFactor(4096);
+  
+  bottomleftMotor.getPIDController().setP(CanSparkMotorP);
+  bottomleftMotor.getPIDController().setI(CanSparkMotorI);
+  bottomleftMotor.getPIDController().setD(CanSparkMotorD);
+  bottomleftMotor.getPIDController().setOutputRange(-0.5, 0.5);
+  bottomleftMotor.getEncoder().setPosition(0);
+  bottomleftMotor.getEncoder().setPositionConversionFactor(4096); 
+
+  topRightMotor.getPIDController().setP(CanSparkMotorP);
+  topRightMotor.getPIDController().setI(CanSparkMotorI);
+  topRightMotor.getPIDController().setD(CanSparkMotorD);
+  topRightMotor.getPIDController().setOutputRange(-0.5, 0.5);
+  topRightMotor.getEncoder().setPosition(0);
+  topRightMotor.getEncoder().setPositionConversionFactor(4096); 
+
+  bottomRightMotor.getPIDController().setP(CanSparkMotorP);
+  bottomRightMotor.getPIDController().setI(CanSparkMotorI);
+  bottomRightMotor.getPIDController().setD(CanSparkMotorD);
+  bottomRightMotor.getPIDController().setOutputRange(-0.5, 0.5);
+  bottomRightMotor.getEncoder().setPosition(0);
+  bottomRightMotor.getEncoder().setPositionConversionFactor(4096); 
+  }
   
 
   @Override
@@ -24,3 +57,4 @@ public class Drivetrain extends SubsystemBase
     // This method will be called once per scheduler run
   }
 }
+
