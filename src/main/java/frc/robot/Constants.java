376b318db884baffe83edc1868fe9995e9938970
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2022-01-26 16:28:56 -0800

    intialized motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int robotPortTopLeft = 1;
+    public static final int robotPortBottomLeft = 2;
+    public static final int robotPortTopRight = 3;
+    public static final int robotPortBottomRight = 4;
+        
+    
+}
