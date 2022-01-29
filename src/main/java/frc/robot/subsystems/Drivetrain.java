b76110d2449836e3b81abed27bdf206eada7028c
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2022-01-29 15:47:46 -0800

    smart motion added

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,22 +5,25 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANPIDController.AccelStrategy;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController.AccelStrategy;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase 
 { 
+
   CANSparkMax topleftMotor = new CANSparkMax(Constants.robotPortTopLeft, MotorType.kBrushless);
   CANSparkMax bottomleftMotor = new CANSparkMax(Constants.robotPortBottomLeft, MotorType.kBrushless);
   CANSparkMax topRightMotor = new CANSparkMax(Constants.robotPortTopRight, MotorType.kBrushless);
   CANSparkMax bottomRightMotor = new CANSparkMax(Constants.robotPortBottomRight, MotorType.kBrushless);
   // change PID values later, can't test rn
-  private final double CanSparkMotorP = 0.0000005;
-  private final double CanSparkMotorI= 0.00000000001;
-  private final double CanSparkMotorD= 0.0000003;
+  private final double CanSparkMotorP = 0.0000002;
+  private final double CanSparkMotorI= 0.0000000000;
+  private final double CanSparkMotorD= 0.000000;
   public Drivetrain() {
 
   topleftMotor.getPIDController().setP(CanSparkMotorP);
@@ -29,15 +32,18 @@ public class Drivetrain extends SubsystemBase
   topleftMotor.getPIDController().setOutputRange(-0.5, 0.5);
   topleftMotor.getEncoder().setPosition(0);
   topleftMotor.getEncoder().setPositionConversionFactor(4096);
-  bottomRightMotor.getPIDController().setReference(0, ControlType.kSmartMotion);
-
+  topleftMotor.getPIDController().setReference(0, ControlType.kSmartMotion);
+  topleftMotor.getPIDController().setSmartMotionMaxAccel(250, 0);
+  topleftMotor.getPIDController().setSmartMotionMaxVelocity(125, 0);
+  
   bottomleftMotor.getPIDController().setP(CanSparkMotorP);
   bottomleftMotor.getPIDController().setI(CanSparkMotorI);
   bottomleftMotor.getPIDController().setD(CanSparkMotorD);
   bottomleftMotor.getPIDController().setOutputRange(-0.5, 0.5);
   bottomleftMotor.getEncoder().setPosition(0);
   bottomleftMotor.getEncoder().setPositionConversionFactor(4096);
-  bottomRightMotor.getPIDController().setReference(0, ControlType.kSmartMotion); 
+  bottomleftMotor.follow(topleftMotor);
+   
  
   topRightMotor.getPIDController().setP(CanSparkMotorP);
   topRightMotor.getPIDController().setI(CanSparkMotorI);
@@ -45,7 +51,9 @@ public class Drivetrain extends SubsystemBase
   topRightMotor.getPIDController().setOutputRange(-0.5, 0.5);
   topRightMotor.getEncoder().setPosition(0);
   topRightMotor.getEncoder().setPositionConversionFactor(4096);
-  bottomRightMotor.getPIDController().setReference(0, ControlType.kSmartMotion); 
+  topRightMotor.getPIDController().setReference(0, ControlType.kSmartMotion);
+  topleftMotor.getPIDController().setSmartMotionMaxAccel(250, 0);
+  topleftMotor.getPIDController().setSmartMotionMaxVelocity(125, 0); 
 
   bottomRightMotor.getPIDController().setP(CanSparkMotorP);
   bottomRightMotor.getPIDController().setI(CanSparkMotorI);
@@ -53,12 +61,9 @@ public class Drivetrain extends SubsystemBase
   bottomRightMotor.getPIDController().setOutputRange(-0.5, 0.5);
   bottomRightMotor.getEncoder().setPosition(0);
   bottomRightMotor.getEncoder().setPositionConversionFactor(4096); 
-  bottomRightMotor.getPIDController().setReference(0, ControlType.kSmartMotion);
+  bottomRightMotor.follow(topRightMotor);
  
-  topleftMotor.getPIDController().setReference(0, ControlType.kPosition);
-  bottomleftMotor.getPIDController().setReference(0, ControlType.kPosition);
-  topRightMotor.getPIDController().setReference(0, ControlType.kPosition);
-  bottomRightMotor.getPIDController().setReference(0, ControlType.kPosition);
+ 
   }
 
   
