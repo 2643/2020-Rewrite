33d9ec7417c194906d28b630291f0a9093d9dfc7
Angela-Dang <91097361+Angela-Dang@users.noreply.github.com>
2022-02-06 19:28:53 -0800

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Intake extends SubsystemBase
+{
+
+  /** Creates a new Intake. */
+  TalonSRX intakeMotor = new TalonSRX (0);
+  
+  private static DigitalInput intakeBall = new DigitalInput (Constants.intakeBallChannel);
+
+  public Intake ()
+  {
+    intakeMotor.setNeutralMode (NeutralMode.Coast);
+  }
+
+  public void setSpeed (double speed)
+  {
+    intakeMotor.set (TalonSRXControlMode.PercentOutput, speed);
+  }
+
+  public void isBallThere ()
+  {
+    if (intakeBall.get () == false)
+    {
+	    Constants.intakingBall = true;
+    }
+    else
+    {
+	    Constants.intakingBall = false;
+    }
+  }
+
+  @Override public void periodic ()
+  {
+    isBallThere();
+    // This method will be called once per scheduler run
+  }
+}
