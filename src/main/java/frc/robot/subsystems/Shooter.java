d0f6d2728cde0d24fcf896cbbf9c13d24f01205d
Shashhank12 <59421940+Shashhank12@users.noreply.github.com>
2022-02-08 17:29:23 -0800

    First commit

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,11 +4,40 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-  public Shooter() {}
+  CANSparkMax leftShooter = new CANSparkMax(Constants.leftShooterPort, MotorType.kBrushless);
+  CANSparkMax rightShooter = new CANSparkMax(Constants.rightShooterPort, MotorType.kBrushless);
+
+  double motorP = 0;
+  double motorI = 0;
+  double motorD = 0; 
+  double outputMin = -0.5;
+  double outputMax = 0.5;
+  int PIDSlot = 0;
+  boolean Invertright = true;
+
+  public Shooter() {
+    leftShooter.getPIDController().setP(motorP, PIDSlot);
+    leftShooter.getPIDController().setI(motorI, PIDSlot);
+    leftShooter.getPIDController().setD(motorD, PIDSlot);
+    leftShooter.getPIDController().setOutputRange(outputMin, outputMax);
+
+    rightShooter.follow(leftShooter);
+    rightShooter.setInverted(Invertright);
+  }
+
+  public void setSpeed(double speed)
+  {
+    leftShooter.getPIDController().setReference(speed, ControlType.kVelocity);
+  }
 
   @Override
   public void periodic() {
